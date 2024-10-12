617ad8e64db91091751f80c0e804133fc7a496b0
AthangMasawkar <athang151004@gmail.com>
2024-10-13 01:40:45 +0530

    goats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -135,23 +135,23 @@ export default function Home() {
               </div>
               <div className="text-lg leading-8">
                 <p>
-                  "The case feels durable and I even got a compliment on the
-                  design. Had the case for two and a half months now and{" "}
+                  "The skin feels durable and I even got a compliment on the
+                  design. Had the skin for three and a half months now and{" "}
                   <span className="p-0.5 bg-slate-800 text-white">
                     the image is super clear
                   </span>
-                  , on the case I had before, the image started fading into
+                  , on the skin I had before, the image started fading into
                   yellow-ish color after a couple weeks. Love it."
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
                 <img
                   className="rounded-full h-12 w-12 object-cover"
-                  src="/users/user-1.png"
-                  alt="user"
+                  src="/users/user-6.png"
+                  alt="Messi"
                 />
                 <div className="flex flex-col">
-                  <p className="font-semibold">Jonathan</p>
+                  <p className="font-semibold">Messi</p>
                   <div className="flex gap-1.5 items-center text-zinc-600">
                     <Check className="h-4 w-4 stroke-[3px] text-green-600" />
                     <p className="text-sm">Verified Purchase</p>
@@ -173,7 +173,7 @@ export default function Home() {
                 <p>
                   "I usually keep my phone together with my keys in my pocket
                   and that led to some pretty heavy scratchmarks on all of my
-                  last phone cases. This one, besides a barely noticeable
+                  last phone skins. This one, besides a barely noticeable
                   scratch on the corner,{" "}
                   <span className="p-0.5 bg-slate-800 text-white">
                     looks brand new after about half a year
@@ -184,11 +184,11 @@ export default function Home() {
               <div className="flex gap-4 mt-2">
                 <img
                   className="rounded-full h-12 w-12 object-cover"
-                  src="/users/user-4.jpg"
-                  alt="user"
+                  src="/users/user-7.png"
+                  alt="Ronaldo"
                 />
                 <div className="flex flex-col">
-                  <p className="font-semibold">Josh</p>
+                  <p className="font-semibold">Ronaldo</p>
                   <div className="flex gap-1.5 items-center text-zinc-600">
                     <Check className="h-4 w-4 stroke-[3px] text-green-600" />
                     <p className="text-sm">Verified Purchase</p>
