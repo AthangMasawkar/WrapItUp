b6125dc83d891542965de8c657291a051b86ab1c
AthangMasawkar <athang151004@gmail.com>
2024-10-14 02:46:58 +0530

    edit6

--- src/components/emails/OrderReceivedEmail.tsx
+++ src/components/emails/OrderReceivedEmail.tsx
@@ -26,7 +26,7 @@ const OrderReceivedEmail = ({
   const baseUrl =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : 'https://casecobra.vercel.app'
+      : 'https://wrapitup.vercel.app'
 
   return (
     <Html>
@@ -36,10 +36,10 @@ const OrderReceivedEmail = ({
         <Container style={container}>
           <Section style={message}>
             <Img
-              src={`${baseUrl}/snake-3.png`}
+              src={`${baseUrl}/cat-3.png`}
               width='65'
               height='73'
-              alt='delivery snake'
+              alt='delivery cat'
               style={{ margin: 'auto' }}
             />
             <Heading style={global.heading}>Thank you for your order!</Heading>
